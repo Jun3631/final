e91c160d234afa3920f6d269a245f3b8e2be0992
Jun <et244157@gmail.com>
2022-01-25 15:55:19 +0800

    Initial Commit

---
+++ final/finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalApp.swift
+//  final
+//
+//  Created by Jun3631 on 2022/1/25.
+//
+
+import SwiftUI
+
+@main
+struct finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
